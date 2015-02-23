3e7e7279d0ea5e0a7d8edbfaa52d7cb2780308f8
David Soto Setzke <david.soto@web.de>
2015-02-23 17:36:08 +0100

    include EXPA ID in SOQL search strings

--- salesforce_wrapper.py
+++ salesforce_wrapper.py
@@ -1,52 +1,59 @@
 # coding=utf-8
 
 import simple_salesforce
-import json
+import logging
 
 
 class SalesforceWrapper:
     def __init__(self, _email, _password, _security_token, _sandbox):
         self.sf = simple_salesforce.Salesforce(username=_email, password=_password, security_token=_security_token,
                                                sandbox=_sandbox)
-        self.current_lead_id = 0
-        self.current_account_id = 0
+        self.current_lead_ids = []
         self.current_ep_ids = []
 
     @staticmethod
     def escape_query_argument(query_argument):
-        return query_argument.replace("?", r"\?").replace("&", r"\&").replace("|", r"\|").replace("!", r"\!").replace(
-            "{", r"\{").replace("}", r"\}").replace("^", r"\^").replace("$", r"\$").replace("*", r"\*").replace("+",
-                                                                                                                r"\+").replace(
-            "-", r"\-").replace("~", r"\~").replace("'", r"\'").replace('"', r'\"').replace('\\', r'\\\\')
+        escaped_argument = query_argument.replace("'", r"\'").replace('"', r'\"').replace('\\', r'\\\\').replace("?", r"\?").replace("&", r"\&").replace("|", r"\|").replace("!", r"\!").replace("^", r"\^").replace("$", r"\$").replace("*", r"\*").replace("+", r"\+").replace("-", r"\-").replace("~", r"\~")
+        logging.debug("Escaping {0} to {1}...".format(query_argument, escaped_argument))
+        return escaped_argument
 
-    def does_lead_exist(self, email):
-        query = 'FIND {' + self.escape_query_argument(email) + '} IN EMAIL FIELDS RETURNING LEAD'
-        query_result = self.sf.search(query)
-        if query_result is None:
-            return False
-        else:
-            self.current_lead_id = str(query_result[0]['Id'])
-            return True
+    def does_lead_exist(self, email, expa_id):
+        query = "SELECT Id FROM Lead WHERE Email = '{0}' OR EXPA_ID__c = {1}".format(email,
+                                                                                   expa_id)
+        try:
+            query_result = self.sf.query_all(query)
+            if query_result is None or query_result["totalSize"] == 0:
+                return False
+            else:
+                self.current_lead_ids = []
+                for record in query_result["records"]:
+                    self.current_lead_ids.append(record["Id"])
+                return True
+        except Exception:
+            logging.exception('An error has occured while searching for Salesforce leads!')
 
     def does_account_exist(self, email):
-        query = 'FIND {' + self.escape_query_argument(email) + '} IN EMAIL FIELDS RETURNING ACCOUNT'
-        query_result = self.sf.search(query)
-        if query_result is None:
-            return False
-        else:
-            self.current_account_id = str(query_result[0]['Id'])
-            return True
+        try:
+            query = 'FIND {' + self.escape_query_argument(email) + '} IN EMAIL FIELDS RETURNING Account'
+            query_result = self.sf.search(query)
+            return query_result is not None and query_result['totalSize'] > 0
+        except Exception:
+            logging.exception('An error has occured while searching for Salesforce accounts!')
 
-    def does_ep_exist(self, email):
-        query = "SELECT Id FROM EP__c WHERE E_Mail__c = '{0}'".format(email)
-        query_result = self.sf.query_all(query)
-        if query_result is None or query_result["totalSize"] == 0:
-            return False
-        else:
-            self.current_ep_ids = []
-            for record in query_result["records"]:
-                self.current_ep_ids.append(record["Id"])
-            return True
+    def does_ep_exist(self, email, expa_id):
+        query = "SELECT Id FROM EP__c WHERE E_Mail__c = '{0}' OR EXPA_ID__c = {1}".format(
+            email, expa_id)
+        try:
+            query_result = self.sf.query_all(query)
+            if query_result is None or query_result["totalSize"] == 0:
+                return False
+            else:
+                self.current_ep_ids = []
+                for record in query_result["records"]:
+                    self.current_ep_ids.append(record["Id"])
+                return True
+        except Exception:
+            logging.exception('An error has occured while searching for Salesforce EPs!')
 
     def create_lead(self, profile_dictionary):
         profile_dictionary['RecordTypeId'] = '01220000000MHoeAAG'
@@ -54,7 +61,8 @@ class SalesforceWrapper:
         self.sf.Lead.create(profile_dictionary)
 
     def update_lead(self, profile_dictionary):
-        self.sf.Lead.update(self.current_lead_id, profile_dictionary)
+        for record in self.current_lead_ids:
+            self.sf.Lead.update(record, profile_dictionary)
 
     def update_ep(self, profile_dictionary):
         profile_dictionary.pop("FirstName", None)
